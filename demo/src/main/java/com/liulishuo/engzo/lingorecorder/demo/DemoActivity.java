c205bacd51c09154f9cb2e713feec89b611fa72a
aesean <visilij@gmail.com>
2017-08-02 15:07:13 +0800

    To adapt permissions (#2)

--- demo/src/main/java/com/liulishuo/engzo/lingorecorder/demo/DemoActivity.java
+++ demo/src/main/java/com/liulishuo/engzo/lingorecorder/demo/DemoActivity.java
@@ -1,9 +1,15 @@
 package com.liulishuo.engzo.lingorecorder.demo;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -30,6 +36,8 @@ public class DemoActivity extends AppCompatActivity {
     public static final String FLAC = "androidFlac";
     public static final String SCORER = "localScorer";
     public static final String TIMER = "timer";
+
+    private static final int REQUEST_CODE_PERMISSION = 10010;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -74,7 +82,7 @@ public class DemoActivity extends AppCompatActivity {
             public void onProcessStop(Throwable error, Map<String, AudioProcessor> map) {
                 if (error != null) {
                     resultView.setText(Log.getStackTraceString(error));
-                } else{
+                } else {
                     WavProcessor wavProcessor = (WavProcessor) map.get(WAV);
                     AndroidFlacProcessor flacProcessor = (AndroidFlacProcessor) map.get(FLAC);
                     LocalScorerProcessor scorerProcessor = (LocalScorerProcessor) map.get(SCORER);
@@ -82,7 +90,7 @@ public class DemoActivity extends AppCompatActivity {
                     StringBuilder sb = new StringBuilder();
                     if (wavProcessor != null) {
                         sb.append(String.format("wav file path = %s size = %s\n", wavProcessor.getFilePath()
-                        , formatFileSize(wavProcessor.getFilePath())));
+                                , formatFileSize(wavProcessor.getFilePath())));
                     }
                     if (flacProcessor != null) {
                         sb.append(String.format("flac file path = %s size = %s \n", flacProcessor.getFilePath()
@@ -102,6 +110,9 @@ public class DemoActivity extends AppCompatActivity {
         recordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!checkRecordPermission()) {
+                    return;
+                }
                 // isAvailable 为 false 为录音正在处理时，需要保护避免在这个时候操作录音器
                 if (lingoRecorder.isAvailable()) {
                     if (lingoRecorder.isRecording()) {
@@ -115,6 +126,47 @@ public class DemoActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, R.string.check_permission_fail, Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+            findViewById(R.id.recordBtn).performClick();
+        }
+    }
+
+    private boolean checkRecordPermission() {
+        if (PermissionChecker.checkSelfPermission(DemoActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || PermissionChecker.checkSelfPermission(DemoActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                        shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)) {
+                    new AlertDialog.Builder(DemoActivity.this)
+                            .setTitle(R.string.check_permission_title)
+                            .setMessage(R.string.check_permission_content)
+                            .setCancelable(false)
+                            .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, REQUEST_CODE_PERMISSION);
+                                    }
+                                }
+                            }).show();
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, REQUEST_CODE_PERMISSION);
+                }
+            }
+            return false;
+        }
+        return true;
     }
 
     private static String formatFileSize(String path) {
