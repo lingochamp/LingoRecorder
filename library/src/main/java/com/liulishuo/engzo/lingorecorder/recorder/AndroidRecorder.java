fd8fb760d4d0699b1e637f345eb1a3e32a2c0b45
王聪武 <wcw@liulishuo.com>
2020-07-17 11:28:24 +0800

    feat: 默认开启 aec 处理

--- library/src/main/java/com/liulishuo/engzo/lingorecorder/recorder/AndroidRecorder.java
+++ library/src/main/java/com/liulishuo/engzo/lingorecorder/recorder/AndroidRecorder.java
@@ -3,7 +3,11 @@ package com.liulishuo.engzo.lingorecorder.recorder;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.media.audiofx.AcousticEchoCanceler;
+import android.media.audiofx.AudioEffect;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.liulishuo.engzo.lingorecorder.recorder.exception.RecorderException;
 import com.liulishuo.engzo.lingorecorder.recorder.exception.RecorderGetBufferSizeException;
@@ -11,6 +15,8 @@ import com.liulishuo.engzo.lingorecorder.recorder.exception.RecorderInitExceptio
 import com.liulishuo.engzo.lingorecorder.recorder.exception.RecorderReadException;
 import com.liulishuo.engzo.lingorecorder.recorder.exception.RecorderStartException;
 import com.liulishuo.engzo.lingorecorder.utils.RecorderProperty;
+
+import java.util.UUID;
 
 /**
  * Created by wcw on 4/5/17.
@@ -22,6 +28,8 @@ public class AndroidRecorder implements IRecorder {
     private long payloadSize;
     private int channels;
     private AudioRecord recorder;
+    private AcousticEchoCanceler aec;
+    private static AudioEffect.Descriptor[] cachedEffects;
 
     private final RecorderProperty recorderProperty;
 
@@ -60,7 +68,7 @@ public class AndroidRecorder implements IRecorder {
     public void startRecording() throws Exception {
         int buffSize = getBufferSize();
 
-        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, recorderProperty.getSampleRate(),
+        recorder = new AudioRecord(MediaRecorder.AudioSource.VOICE_COMMUNICATION, recorderProperty.getSampleRate(),
                 channels, audioFormat, buffSize);
 
         if (recorder.getState() != AudioRecord.STATE_INITIALIZED) {
@@ -69,6 +77,21 @@ public class AndroidRecorder implements IRecorder {
 
         payloadSize = 0;
         recorder.startRecording();
+
+        audioSessionId = recorder.getAudioSessionId();
+
+        boolean aecAvailable = isEffectTypeAvailable(AudioEffect.EFFECT_TYPE_AEC,
+                AOSP_ACOUSTIC_ECHO_CANCELER);
+        Log.d("AndroidRecorder", "AEC Available = " + aecAvailable);
+
+        if (aecAvailable) {
+            aec = AcousticEchoCanceler.create(audioSessionId);
+            if (aec == null || aec.setEnabled(true) != AudioEffect.SUCCESS) {
+                Log.e("AndroidRecorder", "enable aec fail");
+            } else {
+                Log.e("AndroidRecorder", "enable aec success");
+            }
+        }
 
         if (recorder.getRecordingState() != AudioRecord.RECORDSTATE_RECORDING) {
             throw new RecorderStartException();
@@ -90,6 +113,9 @@ public class AndroidRecorder implements IRecorder {
         if (recorder != null) {
             recorder.release();
         }
+        if (aec != null) {
+            aec.release();
+        }
     }
 
     @Override
@@ -102,4 +128,36 @@ public class AndroidRecorder implements IRecorder {
     public RecorderProperty getRecordProperty() {
         return recorderProperty;
     }
+
+    public volatile static int audioSessionId = -1;
+
+    private static final UUID AOSP_ACOUSTIC_ECHO_CANCELER =
+            UUID.fromString("bb392ec0-8d4d-11e0-a896-0002a5d5c51b");
+
+    private static boolean isEffectTypeAvailable(UUID effectType, UUID blackListedUuid) {
+        AudioEffect.Descriptor[] effects = getAvailableEffects();
+        if (effects == null) {
+            return false;
+        }
+        for (AudioEffect.Descriptor d : effects) {
+            if (d.type.equals(effectType)) {
+                return !d.uuid.equals(blackListedUuid);
+            }
+        }
+        return false;
+    }
+
+    private static @Nullable
+    AudioEffect.Descriptor[] getAvailableEffects() {
+        if (cachedEffects != null) {
+            return cachedEffects;
+        }
+        // The caching is best effort only - if this method is called from several
+        // threads in parallel, they may end up doing the underlying OS call
+        // multiple times. It's normally only called on one thread so there's no
+        // real need to optimize for the multiple threads case.
+        cachedEffects = AudioEffect.queryEffects();
+        return cachedEffects;
+    }
+
 }
